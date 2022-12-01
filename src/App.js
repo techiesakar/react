b062998924a3de1c472f3fdc96306076a1019be9
Sakar Aryal <techiesakar@gmail.com>
2022-12-01 14:48:34 +0545

    Todo Add Feature Added - Using useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 function App() {
-  return (
-    <div className="APP">
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
 
+  const addTask = () => {
+    setTodoList([...todoList, newTask]);
+  };
+  return (
+    <div className="App">
+      <div className="addTask">
+        <input type="text" onChange={handleChange} />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="list">
+        {todoList.map((task, key) => {
+          return <h1 key={key}>{task}</h1>;
+        })}
+      </div>
     </div>
   );
 }
