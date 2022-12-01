b26466c19a39011d367fb7e33f6a0a44e944b560
Sakar Aryal <techiesakar@gmail.com>
2022-12-01 09:24:10 +0545

    Working with useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [age, setAge] = useState(0);
+  const increaseAge = () => {
+    setAge(age + 1);
+  };
   return (
     <div className="App">
-     
+      <span>Age is: {age}</span>
+      <button onClick={increaseAge}>Increase Age</button>
     </div>
   );
 }
