7907c0ecb5f9248b371ddbf3440dec7aaf46d0ff
Sakar Aryal <techiesakar@gmail.com>
2022-12-01 12:35:39 +0545

    Crud Started

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
 import "./App.css";
-import { useState } from "react";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        Increase
-      </button>
-      <button
-        onClick={() => {
-          setCount(count - 1);
-        }}
-      >
-        Decrease
-      </button>
-      <button
-        onClick={() => {
-          setCount(0);
-        }}
-      >
-        Set to Zero
-      </button>
-      <h4>Count: {count}</h4>
+    <div className="APP">
+
     </div>
   );
 }
